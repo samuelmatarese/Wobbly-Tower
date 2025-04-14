afc81fe04a56e75cf42c4d71044a0abbd413278e
samuelmatarese <samuel.matarese@protonmail.com>
2025-04-14 20:18:54 +0200

    adding resource preloader

--- GameObjects/TowerBlock/TowerBlock.cs
+++ GameObjects/TowerBlock/TowerBlock.cs
@@ -1,3 +1,6 @@
+using System;
+using System.IO;
+using System.Linq;
 using Godot;
 using WobblyTower.Helpers;
 
@@ -5,17 +8,42 @@ public class TowerBlock : RigidBody
 {
 	private MeshInstance _blockMesh;
 	private Material _blockMaterial;
+	private ResourcePreloader _resourcePreloader = new ResourcePreloader();
 
 	public override void _Ready()
 	{
-		var blockScene = (PackedScene)GD.Load("res://Assets/TowerShapes/tower_block_1.glb");
-		var material = GD.Load("res://Assets/TowerShapes/Material.material");
-		var block = blockScene.Instance();
+		var block = GetRandomBlock();
 
-		_blockMaterial = (Material)material;
+		_blockMaterial = GetRandomMaterial();
 		_blockMesh = NodeExtractionHelper.GetChild<MeshInstance>(block);
 		_blockMesh.MaterialOverride = _blockMaterial;
 
 		AddChild(block);
+	}
+
+	private Node GetRandomBlock()
+	{
+		var blockShapes =_resourcePreloader.GetResourceList()
+			.Where(r => r.StartsWith("tower_block"))
+			.ToArray();
+
+		GD.Print(blockShapes.Length);
+		var random = new Random();
+		var selectedBlock = random.Next(0, blockShapes.Length);
+		var blockScene = (PackedScene)GD.Load(blockShapes[selectedBlock]);
+
+		return blockScene.Instance();
+	}
+
+	private Material GetRandomMaterial()
+	{
+		var materialFiles = _resourcePreloader.GetResourceList()
+			.Where(r => r.StartsWith("block_material"))
+			.ToArray();
+
+		var random = new Random();
+		var selectedBlock = random.Next(0, materialFiles.Length);
+
+		return (Material)GD.Load(materialFiles[selectedBlock]);
 	}
 }
